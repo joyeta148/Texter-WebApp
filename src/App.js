38f97b7930f883d49ca65bd9e781d02be8a28651
Joyeta <joyetahpr@gmail.com>
2022-03-13 01:16:47 +0530

    some files added some removed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [input, setInput] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello Texter ! ✨🙋‍♀️✨</h1>
+
+      <input value={input} onChange={event => setInput(event.target.value)} />
+      <button>Send Message</button>
+      {/* input field */}
+      {/* button */}
+      {/* messages themselves */}
     </div>
   );
 }
